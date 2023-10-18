53b2f1a656187948259448ccce4bd06f757d6952
Mohamed Taha <Taha_Acme@hotmail.com>
2023-10-18 18:21:57 +0200

    lab done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,44 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){
+      return undefined
+    }
+    let len = this.cards.length;
+    while (len > 0) {
+      len--;
+      let temp = this.cards[len];
+      let rdmInd = Math.floor(Math.random() * len);
+      this.cards[len] = this.cards[rdmInd];
+      this.cards[rdmInd] = temp;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+    if(card1 && card2){
+      this.pairsClicked += 1
+      if(card1 === card2){
+      this.pairsGuessed +=1
+      return true
+      } else {
+        return false
+      }
+    } 
+    }
+  
 
   checkIfFinished() {
-    // ... write your code here
+ if (this.pairsGuessed === this.cards.length/2 ){
+  return true
+ } else {
+  return false
+ }
   }
 }
